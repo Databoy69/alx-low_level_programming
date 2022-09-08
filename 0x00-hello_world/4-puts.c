713b512f713bfb826ab5313ee66ef96aa46a59e8
Databoy69 <potuya8@gmail.com>
2022-09-08 12:10:27 -0700

    script that generates the assembly code of a C code and save it in an output file

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,14 +1,1 @@
-
-#include <stdio.h>
-/**
- * main - entry point 
- *
- * return: return 0 after printing the  function 
- */
-int main(void)
-
-{put(*\*Programing is like building a multilingual puzzal*);
-	return (0);
-}
-
-
+#!/bin/bash
