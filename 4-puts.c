3d205188d0f73179911d23e7e8ca31ee710d54ca
Databoy69 <112910491+Databoy69@users.noreply.github.com>
2022-09-08 19:09:51 +0100

    Update 4-puts.c
    Programming is like building a multilingual puzzle, followed by a new line

--- 4-puts.c
+++ 4-puts.c
@@ -1,1 +1,12 @@
+#include <stdio.h>
+/**
+* main -entry point 
+*
+*Return: 0 after printing the function 
+*/
+
+int main(void)
+{Puts("\"Programming is like building a mutillingual puzzel");
+ return (0);
+}
 
