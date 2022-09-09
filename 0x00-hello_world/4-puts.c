8b0901b576c22313f0eecd2cc490d976942421b6
Databoy69 <potuya8@gmail.com>
2022-09-09 07:04:53 -0700

    tman

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,27 +1,16 @@
-
 #include <stdio.h>
+
 /**
-main - print the string in the put function 
-
-Description: using the main function 
-
-this program prints "programming is like building a mutillingual puzzle
-Return: 0
-*/
+ * main - Entry point 
+ * Description: betty is a baby
+ *
+ * return: Always 0 (success)
+ * */
 int main(void)
 {
-puts("\"programming  is like building a mutillingual puzzle");
-
-return (0);
+	puts("\"rogramming is like building a multilingual puzzle");
+	return (0);
 }
-^C
-c
 
-
-
-:wq
-^:wq
-
-^c
-
-
+ *
+ *
