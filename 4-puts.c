d83c68e6519445caa92b3dd7a9cf801ffbc6ceeb
Databoy69 <112910491+Databoy69@users.noreply.github.com>
2022-09-09 01:44:48 +0100

    Update 4-puts.c
     C program that prints exactly "Programming is like building a multilingual puzzle, followed by a new line

--- 4-puts.c
+++ 4-puts.c
@@ -1,14 +1,25 @@
-#include <stdio.h>
-/**
-* main - print the string in the put function
-Description: using the main function
-* this program prints "Programming is like building a mutillingual puzzel
-*Return: 0 
-*/
+#include <stdio.h>                                                                                           
+/**                                                                                                          
+                                                                                                             
+* main - print  the string in the out functions                                                              
+                                                                                                             
+*                                                                                                            
+                                                                                                             
+* Description: using the main function                                                                       
+                                                                                                             
+* this program  prints "Programming is like building a multilingual puzzle                                   
+                                                                                                             
+* Return                                                                                                     
+                                                                                                             
+*/                                                                                                           
+                                                                                                             
+int main(void)                                                                                               
+                                                                                                             
+{                                                                                                            
+                                                                                                             
+puts("\"Programming is like building a multilingual puzzle");                                                
+                                                                                                             
+}               
 
-int main(void)
-{Puts("\"Programming is like building a mutillingual puzzel");
- return (0);
-}
 
 
