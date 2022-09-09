591843208ab9320328e7e6d1fc3b6ec84a9dcdbf
Databoy69 <potuya8@gmail.com>
2022-09-09 07:57:56 -0700

    tm

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,15 +2,14 @@
 
 /**
  * main - Entry point 
- * Description: betty is a baby
  *
- * return: Always 0 (success)
- * */
+ * Return: Always 0 (success)
+ *
+ */
+
 int main(void)
 {
-	puts("\"rogramming is like building a multilingual puzzle");
-	return (0);
-}
+puts("\"rogramming is like building a multilingual puzzle");
 
- *
- *
+return (0);
+}
