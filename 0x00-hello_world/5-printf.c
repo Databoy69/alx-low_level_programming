0d9d5cdf759e28471ffe5396d91fa5ca5c259738
Databoy69 <potuya8@gmail.com>
2022-09-08 21:34:57 -0700

    C program that prints exactly with proper grammar

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+/**
+main - print the string in the put function 
+
+Description: using the main function 
+this program prints "programming is like building a multillingual puzzle
+Return: 0
+*/
+int main(void)
+{
+printf(with proper grammar, but the outcome is a place of art,\n");
+return (0);
