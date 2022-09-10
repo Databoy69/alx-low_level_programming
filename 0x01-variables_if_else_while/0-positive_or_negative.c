cf6106aaec7f9347921f70dc5a85bdbc7ec7cc39
Databoy69 <112910491+Databoy69@users.noreply.github.com>
2022-09-10 09:10:04 +0100

    Update 0-positive_or_negative.c

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -13,9 +13,6 @@ int main(void)
 
         int n;
 
-
-
-<<<<<<< HEAD
         srand(time(0));
 
         n = rand() - RAND_MAX / 2;
@@ -34,28 +31,3 @@ int main(void)
         }
         printf("\n");
    return (0);
-=======
-	srand(time(0));
-
-	n = rand() - RAND_MAX / 2;
-
-	if (n > 0)
-	{
-			printf("%d is positive",n);
-	}
-	else if (n==0)
-	{
-			printf("%d is zero",n);
-	}
-	else
-	{
-			printf("%d is negative",n);
-	}
-	printf("\n");
-	
-
-	return (0);
->>>>>>> 58bdb9440def53e2ac3a670b26bc0600d360ae5b
-
-}
-
