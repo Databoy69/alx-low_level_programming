22cfd84de27f3f82ade1e43f4626f4e8704566d1
Databoy69 <potuya8@gmail.com>
2022-09-20 09:31:26 -0700

    This report, by its very length, defends itself against the risk of being read

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,6 +1,8 @@
+#include <stddef.h>
+
 void reset_to_98(int *n);
         void swap_int(int *a, int *b);
-	int _strlen(char *s);
+	size_t _strlen(const char *str);
 	void _puts(char *str);
 	int _putchar(char c);
 	void print_rev(char *s);
